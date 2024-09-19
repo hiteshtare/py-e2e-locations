97b7d0d0dd7923d3fe03ce6db68831f5decac8b9
Hitesh Tare <hiteshtare@gmail.com>
2024-09-19 17:04:15 +0530

    Added support for Address section

--- cypress/e2e/pages/accommodation.page.ts
+++ cypress/e2e/pages/accommodation.page.ts
@@ -9,15 +9,15 @@ export class AccommodationPages {
   divTitle = '.elementor-heading-title';
 
   mappingPlaceFormId = {
-    ranchi: '_51',
+    ranchi: '_55',
     dakshineswar: '_53'
   };
 
   //Step 1 - Form fields
   inputArrivalDate = '#input_<FORM_ID>_9';
   inputDepartureDate = '#input_<FORM_ID>_12';
-  selectArrivalTime = '#input_<FORM_ID>_14';
-  selectDepartureTime = '#input_<FORM_ID>_13';
+  selectArrivalTime = '#input_<FORM_ID>_58';
+  selectDepartureTime = '#input_<FORM_ID>_60';
   selectPurposeofVisit = '#input_<FORM_ID>_15';
   inputAdditionalInformation = '#input_<FORM_ID>_16';
   checkbox_AccommodationShared = '#choice_<FORM_ID>_44_1';
@@ -28,25 +28,36 @@ export class AccommodationPages {
   divValidation = '#gform_<FORM_ID>_validation_container';
 
   //Step 2 - Form fields
-  inputName = '#input_<FORM_ID>_18';
+  selectSalution = '#input_<FORM_ID>_72';
+  inputFirstName = '#input_<FORM_ID>_18';
+  inputLastName = '#input_<FORM_ID>_64';
+  inputAge = '#input_<FORM_ID>_22';
+  selectGender = '#input_<FORM_ID>_25';
+  selectCountryCode = '#input_<FORM_ID>_71';
   inputMobile = '#input_<FORM_ID>_57';
   inputEmail = '#input_<FORM_ID>_55';
-  selectGender = '#input_<FORM_ID>_25';
-  inputAge = '#input_<FORM_ID>_22';
-  selectCountry = '#input_<FORM_ID>_24';
-  inputCity = '#input_<FORM_ID>_26';
   selectIndianCitizen = '#input_<FORM_ID>_28';
   selectYSS_SRFMember = '#input_<FORM_ID>_27';
   inputLesson = '#input_<FORM_ID>_29';
   selectKriyaban = '#input_<FORM_ID>_31';
+
+  inputLine1 = '#input_<FORM_ID>_68';
+  inputLine2 = '#input_<FORM_ID>_69';
+  inputCity = '#input_<FORM_ID>_26';
+  inputPincode = '#input_<FORM_ID>_65';
+  selectDistrict = '#input_<FORM_ID>_70';
+  selectCountry = '#input_<FORM_ID>_24';
+
   checkbox_agreeTC = '#input_<FORM_ID>_38_1';
-  
+
   btnSubmit = '#gform_submit_button_<FORM_ID>';
 
   /* --------------------- Add Member --------------------- */
   btnAddMember = '.gpnf-add-entry';
   
-  modal_inputName = '#input_52_1';
+  modal_selectSalutation = '#input_52_1';
+  modal_inputFirstName = '#input_52_1';
+  modal_inputLastName = '#input_52_1';
   modal_selectGender = '#input_52_6';
   modal_inputAge = '#input_52_5';
   modal_selectRelationship = '#input_52_3';
@@ -86,21 +97,22 @@ export class AccommodationPages {
     captureSreenshot();
   }
 
-  populateStep2_fields(formId:string) { 
-    cy.get(this.inputName.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_NAME"));
-   
-    cy.get(this.inputMobile.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_MOBILE"));
-    cy.get(this.inputEmail.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_EMAIL"));
+  populateStep2_fields(formId: string) { 
     //Set Dropdown value on Form
+    cy.get(this.selectSalution.replace('_<FORM_ID>',formId)).select(Cypress.env("STEP2_SALUTATION"), {
+      force: true,
+    });
+    cy.get(this.inputFirstName.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_FIRST_NAME"));
+    cy.get(this.inputLastName.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_LAST_NAME"));
+    cy.get(this.inputAge.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_AGE"));
     cy.get(this.selectGender.replace('_<FORM_ID>',formId)).select(Cypress.env("STEP2_GENDER"), {
       force: true,
     });
-    cy.get(this.inputAge.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_AGE"));
-   
-    cy.get(this.selectCountry.replace('_<FORM_ID>',formId)).select(Cypress.env("STEP2_COUNTRY"), {
+    cy.get(this.selectCountryCode.replace('_<FORM_ID>',formId)).select(Cypress.env("STEP2_COUNTRY_CODE"), {
       force: true,
     });
-    cy.get(this.inputCity.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_CITY"));
+    cy.get(this.inputMobile.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_MOBILE"));
+    cy.get(this.inputEmail.replace('_<FORM_ID>', formId)).type(Cypress.env("STEP2_EMAIL"));
     cy.get(this.selectIndianCitizen.replace('_<FORM_ID>',formId)).select(Cypress.env("STEP2_INDIAN_CITIZEN"), {
       force: true,
     });
@@ -110,6 +122,17 @@ export class AccommodationPages {
     });
     cy.get(this.inputLesson.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_LESSON"));
     cy.get(this.selectKriyaban.replace('_<FORM_ID>',formId)).select(Cypress.env("STEP2_KRIYABAN"), {
+      force: true,
+    });
+
+    cy.get(this.inputLine1.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_LINE1"));
+    cy.get(this.inputLine2.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_LINE2"));
+    cy.get(this.inputCity.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_CITY"));
+    cy.get(this.inputPincode.replace('_<FORM_ID>',formId)).type(Cypress.env("STEP2_CITY"));
+    cy.get(this.selectDistrict.replace('_<FORM_ID>', formId)).select(Cypress.env("STEP2_DISTRICT"), {
+      force: true,
+    });
+    cy.get(this.selectCountry.replace('_<FORM_ID>', formId)).select(Cypress.env("STEP2_COUNTRY"), {
       force: true,
     });
 
@@ -123,8 +146,12 @@ export class AccommodationPages {
   }
 
   populateModal_fields() {
-    cy.get(this.modal_inputName).type(Cypress.env("MEMBER1_NAME"));
     //Set Dropdown value on Form
+    cy.get(this.modal_selectGender).select(Cypress.env("MEMBER1_SALUTATION"), {
+      force: true,
+    });
+    cy.get(this.modal_inputFirstName).type(Cypress.env("MEMBER1_FIRST_NAME"));
+    cy.get(this.modal_inputLastName).type(Cypress.env("MEMBER1_LAST_NAME"));
     cy.get(this.modal_selectGender).select(Cypress.env("MEMBER1_GENDER"), {
       force: true,
     });
@@ -142,8 +169,12 @@ export class AccommodationPages {
   }
 
   populateModal_fields_2() {
-    cy.get(this.modal_inputName).type(Cypress.env("MEMBER2_NAME"));
     //Set Dropdown value on Form
+    cy.get(this.modal_selectGender).select(Cypress.env("MEMBER2_SALUTATION"), {
+      force: true,
+    });
+    cy.get(this.modal_inputFirstName).type(Cypress.env("MEMBER2_FIRST_NAME"));
+    cy.get(this.modal_inputLastName).type(Cypress.env("MEMBER2_LAST_NAME"));
     cy.get(this.modal_selectGender).select(Cypress.env("MEMBER2_GENDER"), {
       force: true,
     });
@@ -161,8 +192,12 @@ export class AccommodationPages {
   }
 
   populateModal_fields_3() {
-    cy.get(this.modal_inputName).type(Cypress.env("MEMBER3_NAME"));
     //Set Dropdown value on Form
+    cy.get(this.modal_selectGender).select(Cypress.env("MEMBER3_SALUTATION"), {
+      force: true,
+    });
+    cy.get(this.modal_inputFirstName).type(Cypress.env("MEMBER3_FIRST_NAME"));
+    cy.get(this.modal_inputLastName).type(Cypress.env("MEMBER3_LAST_NAME"));
     cy.get(this.modal_selectGender).select(Cypress.env("MEMBER3_GENDER"), {
       force: true,
     });
