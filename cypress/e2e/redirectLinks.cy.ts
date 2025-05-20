29a0f03a5283ea3d5e1ad84b5639c9b0d1e8e76a
Hitesh Tare <hiteshtare@gmail.com>
2025-05-20 15:18:18 +0530

    Tested Redirection with Logs

--- cypress/e2e/redirectLinks.cy.ts
+++ cypress/e2e/redirectLinks.cy.ts
@@ -1,27 +1,37 @@
 // Import pages
 import { ItemPages } from "./pages/item.page";
-import {
-  captureSreenshot,
-} from "./util/common.util";
+import { captureSreenshot } from "./util/common.util";
 
 // Import custom modules
 const itemPages = new ItemPages();
 
+//  ------- teachings-library data for testing redirection -------  //
+// const testDataForItems = require("../fixtures/teachings-library_redirect_url.json");
+//  ------- teachings-library data for testing redirection -------  //
 //Import test data for Books
 const testDataForItems = require("../fixtures/existing_centres_url.json");
 
-// using baseURL 
-const baseUrl = Cypress.config('baseUrl');
+// using baseURL
+const baseUrl = Cypress.config("baseUrl");
 
 describe.skip("Redirection test for Old Centres links", () => {
   testDataForItems.forEach((testCase: any, index: number) => {
-      it(`#${index + 1} Centre link: ${
-        testCase.match_url
-      } should give 301 REDIRECT status`, () => {
-        const fullURL = `${baseUrl}${testCase.match_url}`;
-        cy.log(`#${index + 1} Centre with Redirection link: ${fullURL}`);
-
-        cy.request("HEAD", testCase.match_url).its("status").should("eq", 301);
+    it(`#${index + 1} Centre link: ${
+      testCase.match_url
+    } should give 301 REDIRECT status`, () => {
+      const fullURL = `${baseUrl}${testCase.match_url}`;
+      const redirectedLocationURL = `${baseUrl}${testCase.url}`;
+      cy.log(`#${index + 1} Centre with Old link: ${fullURL}`);
+      cy.log(`#${index + 1} Centre with New redirected (Location) link: ${redirectedLocationURL}`);
+      
+      cy.request({
+        url: testCase.match_url,
+        followRedirect: false, // turn off following redirects
+      }).then((resp) => {
+        // redirect status code is 301
+        expect(resp.status).to.eq(301);
+        expect(resp.redirectedToUrl).to.eq(redirectedLocationURL);
       });
+    });
   });
 });
